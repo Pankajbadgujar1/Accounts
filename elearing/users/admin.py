8e3011ba0bd8feefa68067d554ab8aedfc1a92e1
Pankaj kumar <pankaj14badgujar@gmail.com>
2025-02-11 11:43:07 +0530

    working correct

--- elearing/users/admin.py
+++ elearing/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Teacher, Student
 
-# Register your models here.
+@admin.register(Teacher)
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = [ 'T_name', 'Designation', 'Username', 'Pass_word', 'Moblie_number', 'Email', 'DOB', 'Date_of_Joining']
+# Register your models here
